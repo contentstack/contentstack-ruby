02977be607f26bf0f068a48b72f16b9625d88c97
Amit Erandole <amit.erandole@gmail.com>
2016-12-19 11:11:44 +0530

    added vcr setup for recording http responses for contentstack

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,8 @@ require 'dotenv'
 Dotenv.load
 WebMock.disable_net_connect!(allow_localhost: true)
 
+Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each { |f| require f }
+
 RSpec.configure do |config|
   config.before(:each) do
     stub_request(:get, /api.github.com/).
