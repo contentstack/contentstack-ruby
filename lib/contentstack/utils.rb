91c41b13236ec69f564dfd457e46be39506ccea3
Amit Erandole <amit.erandole@gmail.com>
2016-12-20 20:10:11 +0530

    utils refactored

--- lib/contentstack/utils.rb
+++ lib/contentstack/utils.rb
@@ -9,8 +9,7 @@ module Contentstack
     # Simple way to turn a deeply nested hash into an openstruct
     # https://coderwall.com/p/74rajw/convert-a-complex-nested-hash-to-an-object
     def to_openstruct(hash)
-      json = hash.to_json
-      JSON.parse(json, object_class: OpenStruct)
+      JSON.parse(hash.to_json, object_class: OpenStruct)
     end
 
   end
