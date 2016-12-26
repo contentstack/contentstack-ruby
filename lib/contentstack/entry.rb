dfa81559a857db8f1ad54e44709dbe73093a996b
Amit Erandole <amit.erandole@gmail.com>
2016-12-26 09:33:33 +0530

    [refactor] lazily instantiating entries

--- lib/contentstack/entry.rb
+++ lib/contentstack/entry.rb
@@ -2,13 +2,22 @@
 
 module Contentstack
   class Entry
+
+    attr_reader :properties, :attributes
     
     def initialize(attributes)
       @attributes = attributes
+      @properties = attributes.keys
+
+      properties.each do |prop|
+        define_singleton_method prop do
+          @attributes[prop]
+        end
+      end
     end
 
-    def properties
-      @attributes.keys
+    def to_s
+      "#{attributes[:title]} created on #{attributes[:created_at]}"
     end
 
     def is_entry?
