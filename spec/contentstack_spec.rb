0d90d49997b2753525ea6a634528739e87dcb16f
sunil-lakshman <104969541+sunil-lakshman@users.noreply.github.com>
2024-05-07 13:08:14 +0530

    Enh/dx 81 gcp na support (#37)
    * CODEOWNERS update
    
    * Update release-gem.yml
    
    * Update release-gem.yml
    
    * Update release-gem.yml
    
    * Added GCP NA support with testcases
    
    * Removed redundancy yml files
    
    ---------
    
    Co-authored-by: Shailesh Mishra <mobile@contentstack.com>
    Co-authored-by: aravindbuilt <aravind.kumar@contentstack.com>

--- spec/contentstack_spec.rb
+++ spec/contentstack_spec.rb
@@ -9,6 +9,7 @@ describe Contentstack do
   let(:custom_host_eu_client) { create_client('DELIVERY_TOKEN_TOKEN', 'API_KEY', 'STACK_ENV', {host: "contentstack.com", region: Contentstack::Region::EU}) }
   let(:custom_host_azure_eu_client) { create_client('DELIVERY_TOKEN_TOKEN', 'API_KEY', 'STACK_ENV', {host: "contentstack.com", region: Contentstack::Region::AZURE_EU}) }
   let(:custom_host_azure_na_client) { create_client('DELIVERY_TOKEN_TOKEN', 'API_KEY', 'STACK_ENV', {host: "contentstack.com", region: Contentstack::Region::AZURE_NA}) }
+  let(:custom_host_gcp_na_client) { create_client('DELIVERY_TOKEN_TOKEN', 'API_KEY', 'STACK_ENV', {host: "contentstack.com", region: Contentstack::Region::GCP_NA}) }
 
   it "has a version number" do
     expect(Contentstack::VERSION).not_to be nil
@@ -23,42 +24,46 @@ describe Contentstack do
 
   it "has default host and region" do
     expect(client.region).to eq Contentstack::Region::US
-    expect(client.host).to eq 'https://cdn.contentstack.io'  
+    expect(client.host).to eq 'https://cdn.contentstack.io'
   end
 
   it "has custom region with region host" do
     expect(eu_client.region).to eq Contentstack::Region::EU
-    expect(eu_client.host).to eq 'https://eu-cdn.contentstack.com'  
+    expect(eu_client.host).to eq 'https://eu-cdn.contentstack.com'
   end
 
   it "has custom region with region host" do
     expect(azure_na_client.region).to eq Contentstack::Region::AZURE_NA
-    expect(azure_na_client.host).to eq 'https://azure-na-cdn.contentstack.com'  
+    expect(azure_na_client.host).to eq 'https://azure-na-cdn.contentstack.com'
   end
 
   it "has custom region with region host" do
     expect(azure_eu_client.region).to eq Contentstack::Region::AZURE_EU
-    expect(azure_eu_client.host).to eq 'https://azure-eu-cdn.contentstack.com'  
+    expect(azure_eu_client.host).to eq 'https://azure-eu-cdn.contentstack.com'
   end
 
   it "has custom host and eu region" do
-    expect(custom_host_eu_client.host).to eq 'https://eu-cdn.contentstack.com'  
+    expect(custom_host_eu_client.host).to eq 'https://eu-cdn.contentstack.com'
   end
 
   it "has custom host and azure-eu region" do
-    expect(custom_host_azure_eu_client.host).to eq 'https://azure-eu-cdn.contentstack.com'  
+    expect(custom_host_azure_eu_client.host).to eq 'https://azure-eu-cdn.contentstack.com'
   end
 
   it "has custom host and azure-na region" do
-    expect(custom_host_azure_na_client.host).to eq 'https://azure-na-cdn.contentstack.com'  
+    expect(custom_host_azure_na_client.host).to eq 'https://azure-na-cdn.contentstack.com'
+  end
+
+  it "has custom host and gcp-na region" do
+    expect(custom_host_gcp_na_client.host).to eq 'https://gcp-na-cdn.contentstack.com'
   end
 
 
   it "JSON to HTML" do
-    expect(Contentstack::json_to_html({}, ContentstackUtils::Model::Options.new())).to eq ''  
+    expect(Contentstack::json_to_html({}, ContentstackUtils::Model::Options.new())).to eq ''
   end
 
   it "JSON to HTML" do
-    expect(Contentstack::render_content('', ContentstackUtils::Model::Options.new())).to eq ''  
+    expect(Contentstack::render_content('', ContentstackUtils::Model::Options.new())).to eq ''
   end
 end
